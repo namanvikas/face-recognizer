3390d2adfc00ad2936eb1cd03164f62180f460c1
namanvikas <79072465+namanvikas@users.noreply.github.com>
2021-05-18 13:25:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,12 +10,12 @@ camera=document.getElementById("camera");
 
 Webcam.attach( '#camera' );
 function snapshot() {
-    Webcam.snap(function (data_uri) { document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>'; }); }
+     Webcam.snap(function (data_uri) { document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>'; }); }
 
+    
 
-    console.log("ml5 version",ml5.version);
-    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/reeAzepxC/model.json",modelloaded)
-;
+console.log("ml5 version",ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/MfT2G4SYy/model.jason",modelloaded);
 
 function modelloaded(){
     console.log("model loaded");
@@ -24,8 +24,6 @@ function modelloaded(){
 function check(){
     img=document.getElementById("captured_image");
     classifier.classify(img,gotResults);
-    
-
 }
 
 function gotResults(error,Results){
@@ -37,4 +35,4 @@ function gotResults(error,Results){
         document.getElementById("result_object_name").innerHTML=Results[0].label;
         document.getElementById("result_object_accuracy").innerHTML=Results[0].confidence.toFixed(3);
     }
-}
+} 
